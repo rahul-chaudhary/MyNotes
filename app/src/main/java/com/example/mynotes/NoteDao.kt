674a1e684b1dc3670896332e082acfff277b623e
unknown <bossrc64@outlook.com>
2024-03-15 16:15:46 +0530

    Changed return type of getAllNotes to LiveData<List<Note>>

--- app/src/main/java/com/example/mynotes/NoteDao.kt
+++ app/src/main/java/com/example/mynotes/NoteDao.kt
@@ -1,4 +1,21 @@
 package com.example.mynotes
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query("SELECT * FROM notes_table ORDER BY id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+
 }
